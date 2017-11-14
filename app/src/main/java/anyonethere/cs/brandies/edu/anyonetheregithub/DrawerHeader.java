808840f5df65b047022745959527c741850826b4
inspectorfiby <phoebezcy93@gmail.com>
2017-11-14 16:06:48 -0500

    Copied string and java

---
+++ app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/DrawerHeader.java
@@ -0,0 +1,32 @@
+package com.anyonethere;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+/**
+ * Created by chensi on 11/7/17.
+ */
+@NonReusable
+@Layout(R.layout.drawer_header)
+public class DrawerHeader {
+
+    @View(R.id.profileImageView)
+    private ImageView profileImage;
+
+    @View(R.id.nameTxt)
+    private TextView nameTxt;
+
+    @View(R.id.emailTxt)
+    private TextView emailTxt;
+
+    @Resolve
+    private void onResolved() {
+        nameTxt.setText("Student Brandeis");
+        emailTxt.setText("sBrandeis@brandeis.edu");
+    }
+}
--- app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/MainActivity.java
+++ app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/MainActivity.java
@@ -1,19 +1,58 @@
-package anyonethere.cs.brandies.edu.anyonetheregithub;
+package com.anyonethere;
 
-import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.View;
+
+import com.mindorks.placeholderview.PlaceHolderView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PlaceHolderView mDrawerView;
+    private DrawerLayout mDrawer;
+    private Toolbar mToolbar;
+    private PlaceHolderView mGalleryView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String s = "lalala";
-        // make a change
-        System.out.print("Hello");
-        Log.w("test", "test");
+
+        // For create navigational menu
+        mDrawer = (DrawerLayout)findViewById(R.id.drawerLayout);
+        mDrawerView = (PlaceHolderView)findViewById(R.id.drawerView);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
+        setupDrawer();
     }
 
+    private void setupDrawer(){
+        mDrawerView
+                .addView(new DrawerHeader())
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_PROFILE))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_NEWREQUEST))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_MANAGEMENT))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_NOTIFICATIONS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_LOGOUT));
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open_drawer, R.string.close_drawer){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
 }
