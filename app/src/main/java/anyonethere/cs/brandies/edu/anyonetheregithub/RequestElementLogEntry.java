4b526811039ac539a25750861612b05903aac80b
wonter123 <lurunjie@hotmail.com>
2017-11-14 18:57:21 -0500

    log of request

--- app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/RequestElementLogEntry.java
+++ app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/RequestElementLogEntry.java
@@ -1,5 +1,7 @@
 package anyonethere.cs.brandies.edu.anyonetheregithub;
 
+import java.util.Date;
+
 /**
  * Created by lurun on 11/14/2017.
  */
@@ -13,14 +15,16 @@ public class RequestElementLogEntry {
     public int expire;
     public String UserID;
     public int ImageID;
+    public Date date;
 
-    public RequestElementLogEntry(String heading, String description, String reward, int from, int expire, String UserID, int ImageID) {
+    public RequestElementLogEntry(String heading, String description, String reward, int from, int expire, String UserID, Date date) {
         this.heading = heading;
         this.description = description;
         this.reward = reward;
         this.expire = expire;
         this.UserID = UserID;
         this.ImageID = ImageID;
+        this.date = date;
     }
 
 }
