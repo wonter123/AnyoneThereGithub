3b031fd17748e52a5c764a0d174afa96187a0a4d
chensi <b.julia1717@gmail.com>
2017-11-30 10:24:22 -0500

    template commit with bugs

---
+++ app/src/main/java/anyonethere/cs/brandies/edu/anyonetheregithub/MainViewFragment.java
@@ -0,0 +1,21 @@
+package anyonethere.cs.brandies.edu.anyonetheregithub;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chensi on 11/30/17.
+ */
+
+public class MainViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main_fragment, container, false);
+    }
+}
